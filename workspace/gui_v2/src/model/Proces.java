ef8d577846afe4bce34461fe3cead3d50837d41e
thomas.bruneel <thomas.bruneel@hotmail.com>
2018-04-24 11:05:10 +0200

    nodige klassen toegevoegd

---
+++ workspace/gui_v2/src/model/Proces.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Proces {
+	private int pid; 
+	private int lastAccesTime;
+	
+	
+	
+	public Proces(int pid){
+		this.pid=pid;
+	}
+	
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public int getLastAccesTime() {
+		return lastAccesTime;
+	}
+	public void setLastAccesTime(int lastAccesTime) {
+		this.lastAccesTime = lastAccesTime;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
